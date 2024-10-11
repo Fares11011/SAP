8c8f4e6ac62866846b0b47cfa0fc89b89493b5e7
Fares11011 <95536143+Fares11011@users.noreply.github.com>
2024-10-11 14:06:01 +0300

    UPLOAD

---
+++ app.py
@@ -0,0 +1,46 @@
+import pickle
+import tensorflow as tf
+from transformers import DistilBertTokenizerFast
+from langdetect import detect, LangDetectException
+
+#LOAD THE Tokenizer
+with open('sentiment_analysis_tokenizer.pkl', 'rb') as f:
+    tokenizer = pickle.load(f)
+
+# LOAD THE MODEL
+model_parts = []
+for i in range(11): 
+    part_file = f'sentiment_analysis_model_part_{i}.pkl'
+    with open(part_file, 'rb') as f:
+        model_part = pickle.load(f)
+        model_parts.append(model_part)
+
+model = tf.keras.Sequential(model_parts)
+
+# PRIDECTION FN.
+def predict_sentiment(tweet):
+    try:
+        # detect THE LANG
+        language = detect(tweet)
+        if language != 'en':
+            return "Warning: The input text is not in English."
+
+        encoding = tokenizer(tweet, truncation=True, padding=True, return_tensors='tf')
+        logits = model(encoding['input_ids'])
+        predicted_class = tf.argmax(logits, axis=-1).numpy()[0]
+        sentiment = "Positive" if predicted_class == 1 else "Negative"
+        return sentiment
+
+    except LangDetectException:
+        return "Error: Unable to detect language."
+
+test_tweets = [
+    "I love the way this app works, it's absolutely fantastic!",
+    "I can't believe how bad the service was, it ruined my day.",
+    "This is the worst movie I've ever watched, such a waste of time!",
+    "I'm feeling really happy today, everything is going great!",
+    "The product is okay, but I think it could be better."
+]
+
+for tweet in test_tweets:
+    print(f"Tweet: '{tweet}' => Sentiment: {predict_sentiment(tweet)}")
